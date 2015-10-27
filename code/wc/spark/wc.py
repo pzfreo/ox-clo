54eab4858dd4bbb9c5028691e70637e0f914d177
Paul Fremantle <pzfreo@github.com>
2015-10-27 11:40:39 +0000

    update

---
+++ code/wc/spark/wc.py
@@ -0,0 +1,22 @@
+# bin/spark-submit --master yarn-cluster wc.py "hdfs://localhost:54310/usr/hduser/books/*"
+
+
+from pyspark import SparkContext, SparkConf
+import sys
+import unicodedata
+
+conf = SparkConf().setAppName("wordCount")
+sc = SparkContext(conf=conf)
+
+
+books = sc.textFile(sys.argv[1])
+split = books.flatMap(lambda line: line.split())
+ascii = split.map(lambda u: str(unicodedata.normalize('NFKD', u).encode('ascii','ignore')))
+stripped = ascii.map(lambda input: ''.join(filter(str.isalpha, input)))
+numbered = stripped.map(lambda word: (word, 1))
+wordcount = numbered.reduceByKey(lambda a,b: a+b)
+
+for k,v in wordcount.collect():
+  print k,v
+
+sc.stop()
