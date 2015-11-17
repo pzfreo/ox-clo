20d3049d44453f33e5da46b766f1f62ea058de48
Paul Fremantle <pzfreo@github.com>
2015-11-17 16:21:23 +0000

    updates

--- code/wind-analysis/complete/wind-mapper.py
+++ code/wind-analysis/complete/wind-mapper.py
@@ -5,10 +5,10 @@ for line in sys.stdin:
   try:	
     line = line.strip()
     for unpacked in csv.reader([line]):
-      Station_ID,Station_Name,Location_Label,Interval_Minutes,Interval_End_Time,vel,Wind_Direction_Variance_Deg,wdd_string,Ambient_Temperature_Deg_C,Global_Horizontal_Irradiance = unpacked
-      vel = float(vel)
-      results = [Station_ID,  str(vel)] 
-      print("\t".join(results)) #dump line by line to stdout, this is different from KEY,ITERABLE[VALUES] format of java
-
+      stationid,name,location,interval,time,vel,direction_variance,wdd,temp,irradiance = unpacked
+      vel = float(vel) 
+      results = [stationid,  str(vel)] 
+      print("\t".join(results)) 
+      
   except:
     pass
