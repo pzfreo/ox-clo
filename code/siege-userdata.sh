02496364cf4923d10356f88af3fce0aeea0ce8cb
Paul Fremantle <pzfreo@gmail.com>
2017-06-24 10:13:54 +0100

    Update siege-userdata.sh

--- code/siege-userdata.sh
+++ code/siege-userdata.sh
@@ -3,8 +3,12 @@
 set -e -x
 # update the package list
 apt-get update
+apt-get install build-essential wget
 # install node, node package manager and git.
-apt-get -y install siege
+wget http://download.joedog.org/siege/siege-4.0.2.tar.gz
+tar xzvf siege-4.0.2.tar.gz
+cd siege-4.0.2
+configure && make && make install
 # set more file descriptors
 echo "* hard nofile 64000" >> /etc/security/limits.conf
 echo "* soft nofile 64000" >> /etc/security/limits.conf
