8227ab8dd9494d57209db6a015a7ee133af27047
Paul Fremantle <pzfreo@github.com>
2019-07-01 09:22:18 +0100

    wrk

---
+++ code/wrk-userdata.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# verbosity
+set -e -x
+# update the package list
+apt-get update
+# install node, node package manager and git.
+sudo apt-get install -y build-essential libssl-dev git
+cd /home/ubuntu 
+git clone https://github.com/wg/wrk.git wrk
+cd wrk
+make
+sudo cp wrk /usr/local/bin
+# set more file descriptors
+echo "* hard nofile 64000" >> /etc/security/limits.conf
+echo "* soft nofile 64000" >> /etc/security/limits.conf
