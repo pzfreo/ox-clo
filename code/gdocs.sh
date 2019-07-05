345bebaef4396f0d3feafaf15b34875fc4b8f904
Paul Fremantle <pzfreo@github.com>
2019-07-05 09:03:26 +0100

    updated slides and docs

--- code/gdocs.sh
+++ code/gdocs.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-drive pull -y -explicitly-export -export docx -exports-dir ~/repos/ox-clo/lab-source/ -same-exports-dir ~/drive/ox-clo/docs/
-drive pull -y -explicitly-export -export pdf -exports-dir ~/repos/ox-clo/lab-exercises/ -same-exports-dir ~/drive/ox-clo/docs/
-drive pull -y -explicitly-export -export pptx -exports-dir ~/repos/ox-clo/pres-source/ -same-exports-dir ~/drive/ox-clo/slides/
-drive pull -y -explicitly-export -export pdf -exports-dir ~/repos/ox-clo/presentations/ -same-exports-dir ~/drive/ox-clo/slides/
+drive pull -no-prompt -explicitly-export -export docx -exports-dir ~/repos/ox-clo/lab-source/ -same-exports-dir ~/drive/ox-clo/docs/
+drive pull -no-prompt -explicitly-export -export pdf -exports-dir ~/repos/ox-clo/lab-exercises/ -same-exports-dir ~/drive/ox-clo/docs/
+drive pull -no-prompt -explicitly-export -export pptx -exports-dir ~/repos/ox-clo/pres-source/ -same-exports-dir ~/drive/ox-clo/slides/
+drive pull -no-prompt -explicitly-export -export pdf -exports-dir ~/repos/ox-clo/presentations/ -same-exports-dir ~/drive/ox-clo/slides/
 cd ~/repos/ox-clo
 git add .
 git commit -m "updated slides and docs"
