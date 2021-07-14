2e650d8addaf64fdaab90e4d43508420655991de
Paul Fremantle <pzfreo@github.com>
2021-07-14 07:52:28 +0100

    .

--- code/flintrock-jupyter.sh
+++ code/flintrock-jupyter.sh
@@ -1,7 +1,6 @@
-sudo yum install gcc gcc-c++ -y
 sudo pip3 install jupyter 
 export PYSPARK_DRIVER_PYTHON=jupyter
 export PYSPARK_DRIVER_PYTHON_OPTS='notebook --no-browser'
 pyspark --master spark://0.0.0.0:7077 \
-     --packages  org.apache.hadoop:hadoop-aws:2.7.4
+     --packages  org.apache.hadoop:hadoop-aws:3.2.0
 
