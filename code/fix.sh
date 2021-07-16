615c0ac66201e079d435d277947db29fe3252574
Paul Fremantle <pzfreo@github.com>
2021-07-16 15:53:36 +0100

    .

--- code/fix.sh
+++ code/fix.sh
@@ -1,3 +1,5 @@
 echo "c.NotebookApp.use_redirect_file = False">> ~/.jupyter/jupyter_notebook_config.py
 sudo -u oxclo  pip3 install notebook --upgrade
 sudo -u oxclo  pip3 install tornado==5.1.1
+sudo pip3 install notebook --upgrade
+sudo pip3 install tornado==5.1.1
