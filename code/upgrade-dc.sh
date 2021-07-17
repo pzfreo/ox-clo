fb47433e90ff0bd975c389f71391b7a0c3f6cc91
Paul Fremantle <pzfreo@github.com>
2021-07-17 12:33:35 +0100

    .

---
+++ code/upgrade-dc.sh
@@ -0,0 +1,5 @@
+sudo rm /usr/bin/docker-compose
+sudo rm /usr/local/bin/docker-compose
+sudo pip3 uninstall docker-compose -y
+sudo pip3 install docker-compose --upgrade
+sudo -u oxclo pip3 install docker-compose --upgrade
