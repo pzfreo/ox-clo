99246bdb7cc64cfbbb9971563f55bf21e6397a72
Paul Fremantle <pzfreo@github.com>
2021-07-20 15:38:05 +0100

    .

--- code/userdata.sh
+++ code/userdata.sh
@@ -3,23 +3,20 @@
 set -e -x
 # update the package list
 apt-get update
+apt-get install curl -y
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - 
 # install node, node package manager and git.
-apt-get -y install nodejs npm git
-# some node packages including forever expect nodejs to be called node
-ln -s /usr/bin/nodejs /usr/local/bin/node
-# use the node package manager to install express.js and mysql support
-npm install express mysql
-# forever is a daemon for running node.js code
-npm install forever -g
-# change to the ubuntu home directory
+apt-get update
+apt-get -y install nodejs
 cd /home/ubuntu
 # use git to copy the node.js code into the system
 git clone https://github.com/pzfreo/auto-deploy-node-js.git
 cd auto-deploy-node-js
+npm install
 # pass the DB connection parameters into the code
 export DBURL=oxclo.cluster-citfamc1edxs.eu-west-1.rds.amazonaws.com
 export DBUSER=node
 export DBPW=node
 # start the server as a daemon
-forever start --minUptime=1000 --spinSleepTime=1000 clustertest.js 
+npm start
 #that's all
