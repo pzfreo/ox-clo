68cfff792e9ccd787c5984177d18914a7f5b97ae
Paul Fremantle <pzfreo@github.com>
2021-07-23 14:25:00 +0100

    .

--- code/delete-user.sh
+++ code/delete-user.sh
@@ -2,5 +2,7 @@
 set -e -x
 aws iam delete-login-profile --user-name $1 || true
 aws iam remove-user-from-group --user-name $1 --group-name students || true
+aws iam delete-access-key --user-name $1 --access-key-id $(aws iam list-access-keys --user-name $1 | jq -r .AccessKeyMetadata[0].AccessKeyId) || true
+aws iam delete-access-key --user-name $1 --access-key-id $(aws iam list-access-keys --user-name $1 | jq -r .AccessKeyMetadata[0].AccessKeyId) || true
 aws iam delete-user --user-name $1 || true
 
